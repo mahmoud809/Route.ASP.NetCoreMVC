82be0a12b591adbff811269974da6b2f25819821
Mahmoud Salah <elshokhaby1999@gmail.com>
2024-09-07 02:55:38 +0300

    Employee Model - [Business Logic Layer] Before Generic Repository

---
+++ MyDemo.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using MyDemo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDemo.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        //Signature of 5 methods
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee employee);
+        int Update(Employee employee);
+        int Delete(Employee employee);
+    }
+}
---
+++ MyDemo.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,47 @@
+﻿using MyDemo.BLL.Interfaces;
+using MyDemo.DAL.Data.Contexts;
+using MyDemo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDemo.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly MVCDemoDbContext _dbContext;
+
+        public EmployeeRepository(MVCDemoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Employee employee)
+        {
+            _dbContext.Employees.Add(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee employee)
+        {
+            _dbContext.Employees.Remove(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+            => _dbContext.Employees.ToList();
+        
+
+        public Employee GetById(int id)
+        {
+            return _dbContext.Employees.Find(id);
+        }
+
+        public int Update(Employee employee)
+        {
+            _dbContext.Employees.Update(employee);
+            return _dbContext.SaveChanges();
+        }
+    }
+}

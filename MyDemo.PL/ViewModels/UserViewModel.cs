8f2c41c8ff4447b598f3b9095bbbbe15e355c86d
Mahmoud Salah <elshokhaby1999@gmail.com>
2024-09-19 05:00:24 +0300

    User Controller - Index

---
+++ MyDemo.PL/ViewModels/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDemo.PL.ViewModels
+{
+	public class UserViewModel
+	{
+        public string Id { get; set; } //becauese Identity generate it by GUID
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+
+
+        ///If U want to Create Action to Create New User U should initailize Id to be GUID in the Contructor
+        ///public UserViewModel()
+        ///{
+        ///    Id = Guid.NewGuid().ToString();
+        ///}
+
+    }
+}
